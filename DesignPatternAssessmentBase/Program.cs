c6ec5e8ec1d53060e45f036322ed91f4b35761b6
zeMontreuilMITT <zacharie.montreuil@mitt.ca>
2022-10-19 11:26:38 -0500

    Add project files.

---
+++ DesignPatternAssessmentBase/Program.cs
@@ -0,0 +1,7 @@
+﻿/*
+ This assessment is designed to gauge your ability to solve a problem and explain your solution, not necessarily your ability to code in any specific syntax. You will provide  a short demonstration of your planned approach to the problem. You may add as much code, pseudocode, or notes to your project as you wish to aid in explaining your solution. Be as detailed as possible in your explanation; show where and what changes you will make, preferably with names and properties picked out already, and indicate why you are making these changes.
+
+All required changes should be made in the Models that have been already provided, or in a new class. No code needs to be added or changed in Program.cs.
+ */
+
+Console.WriteLine("Good luck!");
